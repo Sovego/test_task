af7f7f68f5050d13e1e6c7b92792c07ad8e14afb
Sovego <egor.sofronov03@gmail.com>
2024-05-04 21:00:49 +0700

    refactoring

--- enums.py
+++ enums.py
@@ -1,6 +1,9 @@
 from enum import Enum
 
 
+# Define an enumeration for the record types in the finance application.
+# This Enum is used to avoid errors due to typos in string literals and make the code more readable and maintainable.
+# The two record types are INCOME and EXPENSE, represented by the Russian words 'доход' and 'расход', respectively.
 class RecordType(Enum):
-    INCOME = 'доход'
-    EXPENSE = 'расход'
+    INCOME = 'доход'  # Represents an income record.
+    EXPENSE = 'расход'  # Represents an expense record.
