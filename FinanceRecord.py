d07c2314e7abd43531b8097cc2dfbc103a715309
Sovego <egor.sofronov03@gmail.com>
2024-05-04 19:38:02 +0700

    doc edit

--- FinanceRecord.py
+++ FinanceRecord.py
@@ -2,25 +2,6 @@ class FinanceRecord:
     """
     A class used to represent a Finance Record.
 
-    ...
-
-    Attributes
-    ----------
-    date : str
-        The date of the finance record
-    record_type : str
-        The type of the finance record (e.g., 'income', 'expense')
-    amount : int
-        The amount of money involved in the finance record
-    description : str
-        A brief description of the finance record
-
-    Methods
-    -------
-    __str__():
-        Returns a string representation of the finance record.
-    to_list():
-        Returns a list representation of the finance record for CSV.
     """
 
     def __init__(self, date, record_type, amount, description):
@@ -39,9 +20,21 @@ class FinanceRecord:
                 A brief description of the finance record
         """
         self.date = date
+        """
+        The date attribute is a string representing the date of the finance record.
+        """
         self.record_type = record_type
+        """
+        The record_type attribute is a string representing the type of the finance record.
+        """
         self.amount = int(amount)
+        """
+        The amount attribute is an integer representing the amount of money involved in the finance record.
+        """
         self.description = description
+        """
+        The description attribute is a string representing a brief description of the finance record.
+        """
 
     def __str__(self):
         """
